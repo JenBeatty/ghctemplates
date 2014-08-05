f2e024bcbc4d359cc149a820744e67685d4d5649
Jennifer Beatty <jlbeatty@me.com>
2014-08-05 16:30:54 -0400

    Updating equal heights js

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,24 @@
 // JavaScript Document
 
-
 $(document).ready(function() {
 	
 	//keep the featured content boxes on the homepage the same height
-	$(".cols-top").equalHeights();
-	$(".cols-bottom").equalHeights();
+	//$(".cols-top").equalHeights();
+	//$(".cols-bottom").equalHeights();
 	
     //enable an active link on top nav items
-	$('.dropdown-toggle').click(function() {
-		var location = $(this).attr('href');
-		window.location.href = location;
-		return false;
-	});
+	//$('.dropdown-toggle').click(function() {
+	//	var location = $(this).attr('href');
+	//	window.location.href = location;
+	//	return false;
+	//});
+	
+$(function() {
+    $('.cols-top').matchHeight();
+});
+
+$(function() {
+    $('.cols-bottom').matchHeight();
+});
 
 });
