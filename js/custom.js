c8dce6bdacb9ff909ad0f78e4611e124f01e4714
Jennifer Beatty <jlbeatty@me.com>
2014-08-05 13:57:36 -0400

    Adding notes

--- js/custom.js
+++ js/custom.js
@@ -2,10 +2,12 @@
 
 
 $(document).ready(function() {
+	
+	//keep the featured content boxes on the homepage the same height
 	$(".cols-top").equalHeights();
 	$(".cols-bottom").equalHeights();
 	
-//enable an active link on top nav items
+    //enable an active link on top nav items
 	$('.dropdown-toggle').click(function() {
 		var location = $(this).attr('href');
 		window.location.href = location;
