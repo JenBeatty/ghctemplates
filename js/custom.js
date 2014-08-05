c14b78cd1cd619fc5e77a88105982c059dcbf2bb
Jennifer Beatty <jlbeatty@me.com>
2014-08-05 13:56:31 -0400

    Updating navigation and layout elements.

--- js/custom.js
+++ js/custom.js
@@ -4,4 +4,12 @@
 $(document).ready(function() {
 	$(".cols-top").equalHeights();
 	$(".cols-bottom").equalHeights();
+	
+//enable an active link on top nav items
+	$('.dropdown-toggle').click(function() {
+		var location = $(this).attr('href');
+		window.location.href = location;
+		return false;
+	});
+
 });
